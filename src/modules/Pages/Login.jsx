86254aad755f026355b6dd8d8d4a0176857b0f88
rohitprajapati032 <032rohitprajapati@gmail.com>
2025-10-15 18:19:08 +0530

    bootstrap

--- src/modules/Pages/Login.jsx
+++ src/modules/Pages/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; // ✅ Required for Toast
 
 const Login = () => {
   const [email, setEmail] = useState("");
